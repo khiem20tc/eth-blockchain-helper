ad657087f0d4df6e4efbba28611b9b8e20094002
khiem20tc <khiem20tc@gmail.com>
2022-11-03 11:26:56 +0700

    feat(npm): complete npm package

--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -12,7 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.BlockchainService = void 0;
 const web3_1 = __importDefault(require("web3"));
 const ethereumjs_tx_1 = require("ethereumjs-tx");
 class BlockchainService {
@@ -76,5 +75,5 @@ class BlockchainService {
         });
     }
 }
-exports.BlockchainService = BlockchainService;
+exports.default = BlockchainService;
 //# sourceMappingURL=index.js.map
