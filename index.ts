553ec9b8fad7b37a070b3b36d95d495dc2d5abdc
khiem20tc <khiem20tc@gmail.com>
2023-02-07 10:47:36 +0700

    feat(decode): add feature decode raw transaction

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 import Web3 from "web3";
-import {default as Tx} from "ethereumjs-tx"
+import {default as Tx} from "ethereumjs-tx";
 
 export default class BlockchainService {
     
-    protected WEB3
-    protected SCA
-    protected gasPrice
-    protected ABI
+  protected WEB3;
+  protected SCA;
+  protected gasPrice;
+  protected ABI;
 
-    /**
+  /**
      * The constructor function is used to initialize the web3 object, gas price, smart contract
      * address, and smart contract ABI
      * @param {string} RPC - The RPC address of the Ethereum node you want to connect to.
@@ -18,56 +18,56 @@ export default class BlockchainService {
      * @param {string} SCA - The address of the smart contract you want to interact with.
      * @param {any} ABI - The ABI of the contract you want to interact with.
      */
-    constructor(RPC:string,gasPrice:number,SCA:string,ABI:any) {
-      this.WEB3 = new Web3(
-        new Web3.providers.HttpProvider(RPC)
-      );
+  constructor(RPC:string,gasPrice:number,SCA:string,ABI:any) {
+    this.WEB3 = new Web3(
+      new Web3.providers.HttpProvider(RPC)
+    );
 
-      this.gasPrice = gasPrice;
-      this.SCA = SCA;
-      this.ABI = ABI;
-    }
+    this.gasPrice = gasPrice;
+    this.SCA = SCA;
+    this.ABI = ABI;
+  }
 
-    /**
+  /**
      * It takes a function name, parameters, and a from address, and returns a raw transaction object
      * @param [funcName] - The name of the function you want to call
      * @param params - The parameters of the function you want to call.
      * @param [from] - The address of the account that will be sending the transaction.
      * @returns A raw transaction object.
      */
-    public async createRaw(funcName="",params,from="",value=0) {
+  public async createRaw(funcName="",params,from="",value=0) {
       
-      let ABI = JSON.parse(JSON.stringify(this.ABI))
+    const ABI = JSON.parse(JSON.stringify(this.ABI));
 
-      const contractDeployed = new this.WEB3.eth.Contract(
-        ABI,
-        this.SCA
-      );
+    const contractDeployed = new this.WEB3.eth.Contract(
+      ABI,
+      this.SCA
+    );
 
-      const dataFunc = await contractDeployed.methods[funcName](
-        ...params
-      ).encodeABI();
+    const dataFunc = await contractDeployed.methods[funcName](
+      ...params
+    ).encodeABI();
 
-      const gasLimit = await contractDeployed.methods[funcName](
-        ...params
-      ).estimateGas({ from })
+    const gasLimit = await contractDeployed.methods[funcName](
+      ...params
+    ).estimateGas({ from });
 
-      const nonce = await this.WEB3.eth.getTransactionCount(from)
+    const nonce = await this.WEB3.eth.getTransactionCount(from);
 
-      const rawTx = {
-        from: from,
-        to: this.SCA,
-        gasLimit,
-        gasPrice: this.gasPrice,
-        nonce: nonce,
-        data: dataFunc,
-        value: value
-      };
+    const rawTx = {
+      from: from,
+      to: this.SCA,
+      gasLimit,
+      gasPrice: this.gasPrice,
+      nonce: nonce,
+      data: dataFunc,
+      value: value
+    };
 
-      return rawTx;
-    }
+    return rawTx;
+  }
 
-    /**
+  /**
      * It takes a raw transaction object, a private key, and a chain ID, and returns a transaction hash
      * and the transaction object
      * @param rawTx - The raw transaction object.
@@ -75,34 +75,65 @@ export default class BlockchainService {
      * @param [chainId=97] - The chain ID of the network you're sending to.
      * @returns The transaction hash and the transaction object.
      */
-    public async signRaw(rawTx = {}, privateKey, chainId=97) {
+  public async signRaw(rawTx = {}, privateKey, chainId=97) {
 
-      privateKey = Buffer.from(privateKey, 'hex')
+    privateKey = Buffer.from(privateKey, "hex");
 
-      var transaction = new Tx(rawTx, {chainId: chainId})
+    const transaction = new Tx(rawTx, {chainId: chainId});
 
-      await transaction.sign(privateKey)
+    await transaction.sign(privateKey);
 
-      let signedTx = '0x' + transaction.serialize().toString('hex')
+    const signedTx = "0x" + transaction.serialize().toString("hex");
 
-      return signedTx;
-    }
+    return signedTx;
+  }
 
-   /**
+  /**
+   * It takes a raw transaction string, and returns an object with the transaction's details
+   * @param {string} raw - The raw transaction string
+   * @param [chainId=97] - The chainId of the network you're on.
+   * @returns The rawTx object is being returned.
+   */
+  public async decodeRaw(raw: string) {
+    const tx = new Tx(raw);
+
+    const [
+      nonce, gasPrice,
+      gasLimit, to,
+      value, data,
+      v, r,
+      s
+    ] = tx.toJSON();
+
+    const rawTx = {
+      nonce, gasPrice,
+      gasLimit, to,
+      value, data,
+      v, r,
+      s, 
+      chainId: tx._chainId,
+      from: "0x" + tx.getSenderAddress().toString("hex"),
+      txHash: "0x" + tx.hash().toString("hex")
+    };
+      
+    return rawTx;
+  }
+
+  /**
     * It sends a signed transaction to the blockchain.
     * @param {string} signedTx - The signed transaction in hex format.
     * @returns The txHash and the tx object.
     */
-    public async sendSignedRaw(signedTx: string) {
+  public async sendSignedRaw(signedTx: string) {
 
-      let txHash = this.WEB3.utils.keccak256(signedTx) // ALIAS
+    const txHash = this.WEB3.utils.keccak256(signedTx); // ALIAS
 
-      const tx = await this.WEB3.eth.sendSignedTransaction(signedTx);
+    const tx = await this.WEB3.eth.sendSignedTransaction(signedTx);
 
-      return {txHash, tx};
-    }
+    return {txHash, tx};
+  }
 
-    /**
+  /**
      * It takes in a function name, parameters, and a from address, and returns the data from the
      * function
      * @param [funcName] - The name of the function you want to call.
@@ -110,67 +141,67 @@ export default class BlockchainService {
      * @param from - The address of the account that will be used to call the function.
      * @returns The return value of the function.
      */
-    public async readFunc(funcName="",params, from) {
+  public async readFunc(funcName="",params, from) {
 
-      let ABI = JSON.parse(JSON.stringify(this.ABI))
+    const ABI = JSON.parse(JSON.stringify(this.ABI));
 
-      const contractDeployed = new this.WEB3.eth.Contract(
-        ABI,
-        this.SCA
-      );
+    const contractDeployed = new this.WEB3.eth.Contract(
+      ABI,
+      this.SCA
+    );
 
-      const dataFunc = await contractDeployed.methods[funcName](
-        ...params
-      ).call({ from });
+    const dataFunc = await contractDeployed.methods[funcName](
+      ...params
+    ).call({ from });
 
-      return dataFunc;
+    return dataFunc;
         
-    }
+  }
 
-    /**
+  /**
      * This function returns the receipt of a transaction
      * @param {string} txHash - The transaction hash of the transaction you want to get the receipt
      * for.
      * @returns The receipt of the transaction.
      */
-    public async getReceipt(txHash: string) {
-        const receipt = this.WEB3.eth.getTransactionReceipt(txHash);
+  public async getReceipt(txHash: string) {
+    const receipt = this.WEB3.eth.getTransactionReceipt(txHash);
 
-        return receipt;
-    }
+    return receipt;
+  }
 
-    /**
+  /**
      * This function returns event logs is published from a smart contract
      * @param {array} topics - The topics of log
      * topics[0]: signature event
      * topics[1-3]: indexed params
      * @returns The event log.
      */
-    public async getEvent(topics, fromBlock=0, toBlock=499) {
-      const event = await this.WEB3.eth.getPastLogs({
-        address: this.SCA, //smart contract address published event
-        topics, 
-        fromBlock,
-        toBlock
-    })
+  public async getEvent(topics, fromBlock=0, toBlock=499) {
+    const event = await this.WEB3.eth.getPastLogs({
+      address: this.SCA, //smart contract address published event
+      topics, 
+      fromBlock,
+      toBlock
+    });
 
     return event;
-    }
+  }
 
-    /**
+  /**
      * It takes the ABI of the contract, the data from the log, and the topics from the log, and
      * returns the decoded log
      * @param [data] - The data parameter is the data that was emitted from the contract.
      * @param topics - An array of values generated by the: event.raw.topics
      * @returns The decoded log.
      */
-    public async decodeLog(data="",topics) {
+  public async decodeLog(data="",topics) {
 
-      let ABI = JSON.parse(JSON.stringify(this.ABI))
+    const ABI = JSON.parse(JSON.stringify(this.ABI));
 
-      const result = await this.WEB3.eth.abi.decodeLog(ABI, data, topics)
+    const result = await this.WEB3.eth.abi.decodeLog(ABI, data, topics);
 
-      return result;
-    }
+    return result;
+  }
 
 }
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const BlockchainService = require('./dist/commonjs').default;
+const BlockchainService = require("./dist/commonjs").default;
 
 const RPC= "https://data-seed-prebsc-2-s1.binance.org:8545/";
 const SCA= "0xBE8ed2602fF818715938e2C45d3f312a45Dda8F5";
@@ -347,15 +347,21 @@ const ABI=[
 const instante = new BlockchainService(RPC,gasPrice,SCA,ABI);
 
 async function main() {
-const raw = await instante.createRaw("newAsset",
-["0x80f5dba5485e7dfa399c856234ebc22b17ab9ac6594a42cc5b66daa5e73e052262724c848c823ae8c4402a29333310c468b6af0c6e54cf07b4c7986100dd42591b", 
-"0x28367816f1f564c2c181013627ca39eed4c41cd9", 
-"3", 
-"76889264", 
-"1"],
-"0xF591BA1879Ad0c4B4048DE714a856138c37a31d3")
+  // create raw transaction
+  const rawTx = await instante.createRaw("newAsset",
+    ["0x80f5dba5485e7dfa399c856234ebc22b17ab9ac6594a42cc5b66daa5e73e052262724c848c823ae8c4402a29333310c468b6af0c6e54cf07b4c7986100dd42591b", 
+      "0x28367816f1f564c2c181013627ca39eed4c41cd9", 
+      "3", 
+      "76889264", 
+      "1"],
+    "0xF591BA1879Ad0c4B4048DE714a856138c37a31d3");
 
-console.log("raw",raw)
+  console.log("rawTx",rawTx);
+
+  // decode raw transaction
+  const decode = await instante.decodeRaw("0xf9012280808094739ec8b040502bf50f3fe3fc96ab5af0803d89fe80b8c4e26b5771490e6b0a1f64e7fd3ccef41e61182dc198732945d88833677850c7fc8656a86d000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000412530b686d44c26c93d0541101f4da47d3db4b6713d6300072b761883d138e89c5631204c6f89fdd398bc5b6702432d556830eb4578f741950ab31722e2c8d87d1b000000000000000000000000000000000000000000000000000000000000001ba0d7b51dcc2c2f0ae1f1b0f54f900f914dea7a31b9f4d9b4ff82420a4a4cd2dbdca007e9d1c43fc375f77f4a092f5bee0700ec096409b5904ec454da6a8bcf28bd9a");
+
+  console.log("decode",decode);
 }
 
-main()
+main();
